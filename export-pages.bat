4135c8f19e042e6665430b619622ce1bcfddf470
laurirasanen <laurirasanen@occasionally.cool>
2020-02-02 23:33:56 +0200

    Add wildcard support to --packages arg

--- export-pages.bat
+++ export-pages.bat
@@ -6,6 +6,6 @@
 	--gamedir "C:\Program Files (x86)\Steam\steamapps\common\Momentum Mod\momentum" ^
 	--mapsdir "maps" ^
 	--overwrite --verbose --url-prefix "./resources" ^
-	--packages "sourceutilvpks/hl2_pak_dir.vpk,sourceutilvpks/hl2mp_pak_dir.vpk,sourceutilvpks/cstrike_pak_dir.vpk,sourceutilvpks/tf2_textures_dir.vpk"
+	--packages "sourceutilvpks/*_dir.vpk"
 
 PAUSE
