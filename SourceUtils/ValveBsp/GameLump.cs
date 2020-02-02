12337c516e76861abee78871778c10ea2f4f5576
laurirasanen <laurirasanen@occasionally.cool>
2020-02-02 21:52:34 +0200

    Fix GameLump size sometimes being wrong

--- SourceUtils/ValveBsp/GameLump.cs
+++ SourceUtils/ValveBsp/GameLump.cs
@@ -65,35 +65,66 @@ namespace SourceUtils
                 EnsureLoaded();
 
                 var item = _items[id];
-                var stream = _bspFile.GetSubStream( item.FileOffset, item.FileLength );
 
-                try
+                // Find index of current lump
+                var keyArray = _items.Keys.ToArray();
+                int index = -1;
+                for ( int i = 0; i < keyArray.Length; i++ )
                 {
-                    LzmaHeader lzmaHeader = LzmaHeader.Read( stream );
-
-                    using ( var compressedStream =
-                        _bspFile.GetSubStream( item.FileOffset + LzmaHeader.Size, lzmaHeader.LzmaSize ) )
+                    if ( keyArray[i] == id )
                     {
-                        using ( var uncompressedStream = new MemoryStream( ( int ) lzmaHeader.ActualSize ) )
+                        index = i;
+                        break;
+                    }
+                }
+
+                // https://developer.valvesoftware.com/wiki/Source_BSP_File_Format#Lump_compression
+                // Get the actual length of the item using offset of the next item.
+                var valueArray = _items.Values.ToArray();
+                var actualLength = item.FileLength;
+                if ( index != -1 )
+                {
+                    if ( index < _items.Count - 1 )
+                    {
+                        // The last game lump is /supposed/ to be a dummy containing nothing but the offset,
+                        // but sometimes the offset is 0 which would result in negative actualLength.
+                        if ( valueArray[index + 1].FileOffset > valueArray[index].FileOffset )
                         {
-                            Decoder decoder = new Decoder();
-                            decoder.SetDecoderProperties( lzmaHeader.Properties );
-                            decoder.Code( compressedStream, uncompressedStream, lzmaHeader.LzmaSize,
-                                lzmaHeader.ActualSize, null );
-                            stream = new MemoryStream( ( int ) stream.Length );
-                            uncompressedStream.Seek( 0, SeekOrigin.Begin );
-                            uncompressedStream.CopyTo( stream );
+                            actualLength = valueArray[index + 1].FileOffset - valueArray[index].FileOffset;
                         }
                     }
+                }
 
-                    stream.Seek( 0, SeekOrigin.Begin );
-                    return stream;
+                var stream = _bspFile.GetSubStream( item.FileOffset, actualLength );
+
+                LzmaHeader lzmaHeader;
+                try
+                {
+                    lzmaHeader = LzmaHeader.Read( stream );
                 }
                 catch ( NotSupportedException e )
                 {
                     stream.Seek( 0, SeekOrigin.Begin );
                     return stream;
                 }
+
+                using ( var compressedStream =
+                        _bspFile.GetSubStream( item.FileOffset + LzmaHeader.Size, lzmaHeader.LzmaSize ) )
+                {
+                    using ( var uncompressedStream = new MemoryStream( ( int )lzmaHeader.ActualSize ) )
+                    {
+                        Decoder decoder = new Decoder();
+                        decoder.SetDecoderProperties( lzmaHeader.Properties );
+                        decoder.Code( compressedStream, uncompressedStream, lzmaHeader.LzmaSize,
+                            lzmaHeader.ActualSize, null );
+                        stream = new MemoryStream( ( int )stream.Length );
+                        uncompressedStream.Seek( 0, SeekOrigin.Begin );
+                        uncompressedStream.CopyTo( stream );
+                    }
+                }
+
+                stream.Seek( 0, SeekOrigin.Begin );
+                return stream;
             }
 
             private void EnsureLoaded()
@@ -107,7 +138,13 @@ namespace SourceUtils
                     using ( var reader = new BinaryReader( _bspFile.GetLumpStream( LumpType ) ) )
                     {
                         var count = reader.ReadInt32();
-                        LumpReader<Item>.ReadLumpFromStream( reader.BaseStream, count, item => _items.Add( GetIdString( item.Id ), item ) );
+                        LumpReader<Item>.ReadLumpFromStream( reader.BaseStream, count, item =>
+                        {
+                            if ( !_items.ContainsKey( GetIdString( item.Id ) ) )
+                            {
+                                _items.Add( GetIdString( item.Id ), item );
+                            }
+                        } );
                     }
                 }
             }
