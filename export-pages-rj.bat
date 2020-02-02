4d5fb7848eea35d1b242865a7c43ff7478a738d8
laurirasanen <laurirasanen@occasionally.cool>
2020-02-02 22:57:45 +0200

    Update export script

--- export-pages-rj.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-"SourceUtils.WebExport\bin\Debug\SourceUtils.WebExport.exe" export ^
-	--maps "rj_bomb" ^
-	--outdir "..\MOMReplayViewer-pages\resources" ^
-	--gamedir "C:\Program Files (x86)\Steam\steamapps\common\Momentum Mod\momentum" ^
-	--mapsdir "maps" ^
-	--overwrite --verbose --url-prefix "/MOMReplayViewer/resources" ^
-	--packages "../hl2/hl2_textures_dir.vpk, ../hl2mp/hl2mp_pak_dir.vpk"
--- export-pages.bat
+++ export-pages.bat
@@ -1,9 +1,11 @@
 @echo off
 
-"SourceUtils.WebExport\bin\Debug\SourceUtils.WebExport.exe" export ^
-	--maps "surf_mesa" ^
-	--outdir "..\MOMReplayViewer-pages\resources" ^
+"SourceUtils.WebExport\bin\x64\Debug\SourceUtils.WebExport.exe" export ^
+	--maps "bhop_*,rj_*,surf_*" ^
+	--outdir "..\MomReplayViewer-pages\resources" ^
 	--gamedir "C:\Program Files (x86)\Steam\steamapps\common\Momentum Mod\momentum" ^
 	--mapsdir "maps" ^
-	--overwrite --verbose --url-prefix "/MOMReplayViewer/resources" ^
-	--packages "../hl2/hl2_textures_dir.vpk, ../hl2mp/hl2mp_pak_dir.vpk"
+	--overwrite --verbose --url-prefix "./resources" ^
+	--packages "sourceutilvpks/hl2_pak_dir.vpk,sourceutilvpks/hl2mp_pak_dir.vpk,sourceutilvpks/cstrike_pak_dir.vpk,sourceutilvpks/tf2_textures_dir.vpk"
+
+PAUSE
